e9415502849dc905db424c4b7f12109eeed82335
dgarg7599 <gargdivyansh7599@gmail.com>
2025-04-07 14:46:57 +0530

    [Divyansh Garg] ADD: Use Suppress Warnings annotation.

---
+++ src/main/java/com/bridgelabz/SuppressWarning.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+import java.util.ArrayList;
+
+public class SuppressWarning {
+
+    public static void main(String[] args) {
+        @SuppressWarnings("unchecked")
+        ArrayList list = new ArrayList();
+    }
+
+}
